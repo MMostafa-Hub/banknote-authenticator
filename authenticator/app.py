055c26db5e42cca089a157007e306e338521c47c
MMostafa-Hub <moh.mostafa.ibra@gmail.com>
2023-11-28 23:33:54 +0200

    Add flask authentication endpoint and load ML model

---
+++ authenticator/app.py
@@ -0,0 +1,29 @@
+from autosklearn.classification import AutoSklearnClassifier
+from flask import Flask, request, jsonify
+from load_model import load_model
+import pandas as pd
+
+# Create the Flask app
+app = Flask(__name__)
+
+# The AutoSklearnClassifier model
+model = None
+
+
+@app.route("/authentication", methods=["GET"])
+def authentication():
+    """Endpoint for authentication.
+
+    Returns:
+        JSON: The authentication result.
+    """
+    global model
+    test_data = pd.Series(request.get_json())
+    return jsonify({"output": model.predict_proba(test_data)[1]}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8400)
+
+    # loading the ml model for inference
+    model = load_model()
---
+++ authenticator/load_model.py
@@ -0,0 +1,10 @@
+from autosklearn.classification import AutoSklearnClassifier
+import pickle
+
+
+def load_model() -> AutoSklearnClassifier:
+    """Load the model object from disk."""
+    with open("../models/model.pkl", "rb") as f:
+        model = pickle.load(f)
+
+    return model
